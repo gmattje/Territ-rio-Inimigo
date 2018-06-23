54a5bf2058503e2de31752c17a99bc8faf46ff5a
gmattje <gmattje@gmail.com>
2018-06-23 19:56:27 -0300

    Alteração servidor online

--- js/game-online.js
+++ js/game-online.js
@@ -1,4 +1,4 @@
-var urlServerTerritorioInimigo = "http://www.minias.com.br/testes/ti/_server";
+var urlServerTerritorioInimigo = "http://www.eventore.com.br/testes/ti/_server";
 var tempoSincronizacao = 3000; //a cada 3 segundos
 var idSessaoServidor = "";
 var ultimoIdServidorPecasTurno = "";
